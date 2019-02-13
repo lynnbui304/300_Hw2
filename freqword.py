1c3fd66b4bb09f63e0556dc7e1dd64cb8dacc518
LynnBui <katherine.bui.304@gmail.com>
2019-02-13 15:26:43 -0800

    newfile

---
+++ freqword.py
@@ -0,0 +1,22 @@
+# freqword.py
+#
+# Written by: Steven Parker and Lynn Bui
+######################
+
+# Code for approximate pattern count
+def
+    pass
+
+
+
+
+# Code for FrequentWordsWithMRC
+def
+    pass
+
+
+
+
+
+
+
