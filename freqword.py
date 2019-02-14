2745316efd9ccd054c89193d6037a51ea4986f4c
Steve <srp2120@gmail.com>
2019-02-13 16:58:03 -0800

    Real Work Today

--- freqword.py
+++ freqword.py
@@ -3,15 +3,38 @@
 # Written by: Steven Parker and Lynn Bui
 ######################
 
-# Code for approximate pattern count
-def
-    pass
+# Hamming Distance function
+def HammingDistance(pat1,pat2):
+    count=0
+    for i in range(len(pat1)):
+        if pat1[i] != pat2[i]:
+            count += 1
+    return count
 
+# Code for approximate pattern count
+def ApproximatePatternCount(text,pattern,d):
+    count = 0
+    patlen = len(pattern)
+    #print("count: "+str(count)+" patlen: "+str(patlen))
+    for i in range(len(text)-patlen):
+        #print("i: "+str(i)+" range: "+str(len(range(len(text)-patlen)))
+        newpat=text[i:patlen+i]
+        #print("newpat: "+newpat)
+        if HammingDistance(pattern,newpat)<=d:
+            count += 1
+            print("index: "+str(i))
+    return count
+    
+    
+a="GATTACATTTATCACACACTTAAGGCTGTGAGCAT" #35
+b="GATTACA"
+app=ApproximatePatternCount(a,b,3)
+print(app)
 
 
 
 # Code for FrequentWordsWithMRC
-def
+def FrequentWordsWithMRC():
     pass
 
 
